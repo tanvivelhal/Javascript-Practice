9269b3f72e9b44f511959bd22cbcb11b3a3240d4
tanvivelhal <tanvivelhal12@gmail.com>
2025-07-01 02:22:50 +0530

    added array related things

---
+++ 03Array/01basic.js
@@ -0,0 +1,28 @@
+let arr = ["tanvi","kirti","suvesh","pranav"]
+
+arr.push("pranav")                    //adds in end of array
+console.log(arr);
+
+
+arr.unshift("sidu")                   //adds in start of array
+console.log(arr);
+
+
+arr.pop()                             //remove last element
+console.log(arr);
+
+
+arr.shift()                           //remove the start element
+console.log(arr);
+
+
+console.log(typeof arr.join());       //convert into strings
+
+console.log(arr.splice(0,3));
+
+
+console.log(arr);
+
+
+
+
---
+++ 03Array/02arrays.js
@@ -0,0 +1,19 @@
+let name1 = ["tanvi", "kirti", "suvesh"]
+let name2 = ["siddhesh", "pranav"]
+
+// const newName = name1.concat(name2)
+// console.log(newName);
+
+// const arr = [...name1, ...name2]
+// console.log(arr)
+
+// let arr = [1,2,3,[4,5,6],7,8,9,[1,2]]
+// console.log(arr.flat(Infinity))
+
+console.log(Array.isArray("suvesh"));
+console.log(Array.from("suvesh"));
+console.log(Array.of("suvesh"));            //stored multiple data
+
+
+
+
