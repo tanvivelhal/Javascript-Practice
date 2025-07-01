7cc26781efef4314ea3d1dd5e514ce951cbe022a
tanvivelhal <tanvivelhal12@gmail.com>
2025-07-02 01:39:00 +0530

    different types of function

---
+++ 05Function/01basic.js
@@ -0,0 +1,53 @@
+function func1(){
+    console.log("s");
+    console.log("u");
+    console.log("v");
+    console.log("e");
+    console.log("s");
+    console.log("h");
+}
+// func1()
+
+
+
+function func2(name1, name2){
+    return (`${name1} and ${name2} are best friend`);
+    
+}
+
+// console.log(func2("suvesh","sidu"))
+
+
+function func3(a,b){
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a / b);
+    console.log(a * b);
+}
+// func3(5,7)
+
+
+
+
+// *************************************************************************************************************
+
+
+
+let obj1 = {
+    name : "tanvi",
+    email : "tanvi@gmail.com",
+    password : "pass@123"
+}
+
+let arr1 = [100,200,800,900]
+
+function funcObj(anyArray){
+    console.log(`my salary is ${anyArray[2]}`);}
+
+funcObj(arr1)
+
+
+// function funcObj(anyObj){
+//     console.log(`my name is ${anyObj.name} and my email is ${anyObj.email} and my password is ${anyObj.password}`);
+// }
+// funcObj(obj1)
